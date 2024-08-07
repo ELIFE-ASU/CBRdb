3cae7dffc73e7b64769ca7c6f0d7923aaecfda0a
Louie Slocombe <louies@hotmail.co.uk>
2024-08-07 16:15:56 -0700

    fixed some issues with inchi converter

--- mol_to_inchi.py
+++ mol_to_inchi.py
@@ -11,13 +11,9 @@ from rdkit.Chem import rdMolDescriptors
 from rdkit.Chem import Draw
 from rdkit import RDLogger
 
-from ase.io import read, write
-from ase.optimize import BFGS
-from mace.calculators import MACECalculator
-from ase.visualize import view
+lg = RDLogger.logger()
+lg.setLevel(RDLogger.CRITICAL)
 
-# lg = RDLogger.logger()
-# lg.setLevel(RDLogger.CRITICAL)
 
 def file_list(mypath=None):
     """
@@ -71,10 +67,12 @@ def replace_r_group(target_file, new_file, re_atom="C", re_target=["R# ", "R ", 
             nf.write(line)
         nf.write("M  END\n\n")
 
+
 def check_for_problem_group(target_file, re_target=["OH"]):
     with open(target_file, "r") as f:
         lines = [line for line in f if "M  " not in line]
         return any(target in line for line in lines for target in re_target)
+
 
 def replace_problem_group(target_file, new_file, re_target=["OH"]):
     with open(target_file, "r") as f, open(new_file, "w") as nf:
@@ -103,10 +101,15 @@ def embed_mol(mol):
     Achem.Kekulize(mol)
     # Add hydrogens
     mol = Chem.AddHs(mol)
+    return mol
+
+
+def standardize_mol(mol):
+    # Standardize the molecule
+    nrm = rdMolStandardize.Normalizer()
+    nrm.normalizeInPlace(mol)
     # Embed the molecule
-    ps = Achem.ETKDGv2()
-    ps.useRandomCoords = True
-    Achem.EmbedMolecule(mol, ps)
+    # mol = embed_mol(mol)
     return mol
 
 
@@ -145,31 +148,26 @@ def delete_files_substring(target_dir, substring):
     return count
 
 
-if __name__ == "__main__":
-    print("Program started", flush=True)
-    target = "C"
-    target_dir = os.path.abspath(r"C:/Users/louie/skunkworks/data/kegg_data_" + target)
+def convert_mol_to_inchi(target_dir, bad_list, man_dict, outfile="kegg_inchi_C.csv"):
+    # Get a list of all files in the directory
     files = file_list_all(target_dir)
     # Clean up the files
     delete_files_substring(target_dir, "mod")
-    # mostly valence errors
-    bad_list = ["C01322","C02202","C00210", "C00462",  "C19040", "C21014", "C22680"]
-    man_dict = {"C02202":"InChI=1S/C27H26N4O4/c28-29-18-25(32)23(16-20-10-4-1-5-11-20)30-26(33)24(17-21-12-6-2-7-13-21)31-27(34)35-19-22-14-8-3-9-15-22/h1-15,18,23-24H,16-17,19H2,(H,30,33)(H,31,34)/t23-,24-/m0/s1",
-                "C13681":"InChI=1S/C8H10N3.BF4/c1-11(2)8-5-3-7(10-9)4-6-8;2-1(3,4)5/h3-6H,1-2H3;/q+1;-1",
-                "C13932":"InChI=1S/6ClH.14H3N.2O.3Ru/h6*1H;14*1H3;;;;;/q;;;;;;;;;;;;;;;;;;;;;;3*+2/p-6",
-                "C18368":"InChI=1S/ClO2/c2-1-3",
-                "C19040":"InChI=1S/F6Si.2Na/c1-7(2,3,4,5)6;;/q-2;2*+1"}
-
     # Filter the files to only include .mol files
     files = [f for f in files if "mod" not in f]
-    arr_inchi = np.empty(len(files)-len(bad_list), dtype=str)
-    cids = np.empty(len(files)-len(bad_list), dtype=str)
-
+    # Create arrays to store the InChI and CID
+    n_data = len(files)
+    arr_inchi = []
+    arr_cid = []
+    # Loop over the files
     for i, file in enumerate(files):
+        # Get the CID
         cid = os.path.basename(file).split(".")[0]
         print(f"Processing file {i + 1}/{len(files)}: {cid}", flush=True)
+        # Check if the CID is in the bad list
         if cid in bad_list:
             continue
+        # Check if the CID is in the manual fix dictionary
         if cid in man_dict:
             mol = Chem.MolFromInchi(man_dict[cid])
         else:
@@ -192,21 +190,31 @@ if __name__ == "__main__":
             remove(f_load_r)
         if flag_p:
             remove(f_load_p)
-        # standardize the molecule
-        nrm = rdMolStandardize.Normalizer()
-        nrm.normalizeInPlace(mol)
-        mol = embed_mol(mol)
+        # Standardize and embed the molecule
+        mol = standardize_mol(mol)
+        # Get the InChI
+        arr_inchi.append(Chem.MolToInchi(mol))
+        arr_cid.append(cid)
 
-        # mol = Achem.MolFromMolFile(file, removeHs=True, sanitize=False)
-        # mol.UpdatePropertyCache(strict=False)
-        # Chem.SanitizeMol(mol,Chem.SanitizeFlags.SANITIZE_FINDRADICALS|Chem.SanitizeFlags.SANITIZE_KEKULIZE|Chem.SanitizeFlags.SANITIZE_SETAROMATICITY|Chem.SanitizeFlags.SANITIZE_SETCONJUGATION|Chem.SanitizeFlags.SANITIZE_SETHYBRIDIZATION|Chem.SanitizeFlags.SANITIZE_SYMMRINGS,catchErrors=True)
-        # img = Draw.MolToImage(mol)
-        # img.show()
+    # Create a dataframe
+    df = pd.DataFrame(data={"CID": arr_cid, "InChI": arr_inchi})
+    # Remove any entries with zero
+    df = df[df["InChI"] != "0"]
+    # Save the dataframe
+    df.to_csv(outfile, index=False)
 
-        arr_inchi[i] = Chem.MolToInchi(mol)
-        cids[i] = cid
-    # create a dataframe
-    df = pd.DataFrame(data={"CID": cids, "InChI": arr_inchi})
-    # save the dataframe
-    df.to_csv(f"kegg_inchi_{target}.csv", index=False)
+
+if __name__ == "__main__":
+    print("Program started", flush=True)
+    target_dir = os.path.abspath(r"C:/Users/louie/skunkworks/data/kegg_data_C")
+    # mostly valence errors
+    bad_list = ["C01322", "C02202", "C00210", "C00462", "C19040", "C21014", "C22680", "C01365", "C01706", "C01812",
+                "C01813"]
+    man_dict = {
+        "C02202": "InChI=1S/C27H26N4O4/c28-29-18-25(32)23(16-20-10-4-1-5-11-20)30-26(33)24(17-21-12-6-2-7-13-21)31-27(34)35-19-22-14-8-3-9-15-22/h1-15,18,23-24H,16-17,19H2,(H,30,33)(H,31,34)/t23-,24-/m0/s1",
+        "C13681": "InChI=1S/C8H10N3.BF4/c1-11(2)8-5-3-7(10-9)4-6-8;2-1(3,4)5/h3-6H,1-2H3;/q+1;-1",
+        "C13932": "InChI=1S/6ClH.14H3N.2O.3Ru/h6*1H;14*1H3;;;;;/q;;;;;;;;;;;;;;;;;;;;;;3*+2/p-6",
+        "C18368": "InChI=1S/ClO2/c2-1-3",
+        "C19040": "InChI=1S/F6Si.2Na/c1-7(2,3,4,5)6;;/q-2;2*+1"}
+    convert_mol_to_inchi(target_dir, bad_list, man_dict)
     print("Program finished", flush=True)
