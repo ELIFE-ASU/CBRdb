0e3c28f4cecf2bef08d7b830f31f40ce78057b31
Louie Slocombe <louies@hotmail.co.uk>
2024-08-08 16:37:19 -0700

    added code to check the reactions

---
+++ fix_reactions_data.py
@@ -0,0 +1,208 @@
+import os
+import time
+import numpy as np
+
+import requests
+from requests import Session
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
+from chempy import balance_stoichiometry
+
+"""
+things to do
+- check if the equation has n
+- check if the equation has a reactant and product side
+- check if the equation balances
+
+
+"""
+
+
+def check_known_ids(id):
+    if id == "C00138":  # Reduced ferredoxin
+        return "Fe2S2"
+    elif id == "C00139":  # Oxidized ferredoxin
+        return "Fe2S2"
+    elif id == "C05359":  # electron
+        return " "
+    else:
+        return False
+
+
+def get_formula(id, kegg_website="https://rest.kegg.jp/get/", request_sleep=0.2):
+    # Check if the id is known
+    # known = check_known_ids(id)
+    # if known:
+    #     return known
+
+    # Make the session
+    s = Session()
+    # Add retries
+    retries = Retry(
+        total=5,
+        backoff_factor=0.1,
+        status_forcelist=[502, 503, 504],
+        allowed_methods={'POST'},
+    )
+    # Mount the session
+    s.mount('https://', HTTPAdapter(max_retries=retries))
+    # Limit the number of requests
+    time.sleep(request_sleep)
+    # Get the data
+    try:
+        response = s.get(f"{kegg_website}{id}", timeout=10.0)
+    except requests.exceptions.RequestException as e:
+        # Some error in the connection
+        print(f"Error in ID {id}, connection exception {e}")
+        return "fucked"
+    # Check if the response is ok
+    if response.ok:
+        # Strip the last section of the file
+        # print(response.text)
+        res = response.text.split("> <ENTRY>")[0]
+        data = res.split("\n")
+        # print(data)
+        # exit()
+        # Get the line which contains the formula
+        data = [line for line in data if "FORMULA" in line]
+        if len(data) == 0:
+            return "fucked"
+        return data[0].split("FORMULA")[1].strip()
+
+    else:
+        # Some error in the response
+        print(f"Error in ID {id}, response {response.status_code}")
+        return "fucked"
+
+
+def file_list(mypath=None):
+    mypath = mypath or os.getcwd()  # If no path is provided, use the current working directory
+    return [f for f in os.listdir(mypath) if
+            os.path.isfile(os.path.join(mypath, f))]  # Return a list of all files in the directory
+
+
+def filter_files(file_paths, substring):
+    return list(filter(lambda file_path: substring in os.path.basename(file_path), file_paths))
+
+
+def sub_file_list(mypath, substring):
+    paths = file_list(mypath)
+    return filter_files(paths, substring)
+
+
+def file_list_all(mypath=None):
+    """
+    This function generates a list of all files in a specified directory and its subdirectories.
+    If no directory is specified, it defaults to the current working directory.
+
+    Parameters:
+    mypath (str, optional): The path to the directory. Defaults to None, which means the current working directory.
+
+    Returns:
+    list: A list of all files in the specified directory and its subdirectories.
+    """
+    mypath = mypath or os.getcwd()  # If no path is provided, use the current working directory
+    files = []
+    # os.walk generates the file names in a directory tree by walking the tree either top-down or bottom-up
+    for dirpath, dirnames, filenames in os.walk(mypath):
+        for filename in filenames:
+            # os.path.join joins one or more path components intelligently
+            files.append(os.path.join(dirpath, filename))
+    return files
+
+
+def select_numbers(arr):
+    return [x for x in arr if x.isdigit()]
+
+
+def get_numbers_of_mols(inpt):
+    return
+
+
+target_dir = r"C:\Users\louie\skunkworks\data\kegg_data_R"
+paths = file_list_all(target_dir)
+
+"""
+fucked buckets
+1) Has an n
+2) Missing mol file (no formula)
+3) Equations do not balance
+"""
+
+fucked_n = []
+fucked_eq = []
+fucked_missing_mol = []
+fucked_no_balance = []
+
+for id, path in enumerate(paths):
+    if id > 30:
+        break
+    re_id = os.path.basename(path).split(".")[0]
+    print(f"Processing {id + 1}/{len(paths)} {re_id}")
+    # load the data
+    with open(path, "r") as f:
+        data = f.read()
+    # Split the data by new lines
+    data = data.split("\n")
+    eq_line = [d for d in data if "EQUATION" in d][0]
+    # Get the line which contains the equation
+    eq_line = eq_line.split("EQUATION")[1].strip()
+    print("Equation line:", eq_line)
+
+    # check if the equation has n
+    if "n" in eq_line:
+        print("Warning! Equation has n")
+        fucked_n.append(re_id)
+        continue
+
+    eq_sides = eq_line.split("<=>")
+    if len(eq_sides) != 2:
+        print("Warning! Equation does not have a reactant and product side")
+        fucked_eq.append(re_id)
+        continue
+
+    tmp1 = eq_sides[0].split("+")
+    print(tmp1)
+
+    # Get all the numbers in front of the equation line
+    tmp1 = eq_sides[0].split()
+    print(tmp1)
+    # count the number of molecules
+    n_mols = len([i for i in tmp1 if str(i) == "+"]) + 1
+    filtered_array = select_numbers(tmp1)
+    print(filtered_array)
+
+    all_ids = [id for id in eq_sides[0].split(" ") if id.startswith("C")]
+    eq_react = [get_formula(id) for id in all_ids]
+    print("Reactant", eq_react)
+    if "fucked" in eq_react:
+        print("Warning! No formula in reactant")
+        fucked_missing_mol.append(re_id)
+        continue
+
+    all_ids = [id for id in eq_sides[1].split(" ") if id.startswith("C")]
+    eq_prod = [get_formula(id) for id in all_ids]
+    print("Product", eq_prod)
+    if "fucked" in eq_prod:
+        print("Warning! No formula in product")
+        fucked_missing_mol.append(re_id)
+        continue
+    # Trying to find
+    try:
+        reac, prod = balance_stoichiometry(eq_react, eq_prod)
+        print(dict(reac))
+        print(dict(prod))
+    except:
+        print("Could not find stoichiometry")
+        fucked_no_balance.append(re_id)
+
+# print out the bad files
+print(f"fucked n: {fucked_n}")
+print(f"fucked eq: {fucked_eq}")
+print(f"fucked_missing_mol: {fucked_missing_mol}")
+print(f"fucked no balance: {fucked_no_balance}")
+# print out the length of each of the lists
+print(f"len fucked n: {len(fucked_n)}")
+print(f"len fucked eq: {len(fucked_eq)}")
+print(f"len fucked_missing_mol: {len(fucked_missing_mol)}")
+print(f"len fucked no balance: {len(fucked_no_balance)}")
