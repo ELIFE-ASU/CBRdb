05f6ffe8135345ff359545f707c2bdf76a992af2
Louie Slocombe <louies@hotmail.co.uk>
2024-08-09 16:41:04 -0700

    updated atlas converter

--- atlas_converter.py
+++ atlas_converter.py
@@ -9,8 +9,40 @@ file_path = r"C:\Users\louie\skunkworks\data\atlas_reactions.csv"
 
 # load the data using pandas
 data = pd.read_csv(file_path)
-print(data.head())
-# get the 
-atlas_ids = list(set(data["rn"]))
+# get the set of the ids
+atlas_ids = sorted(list(set(data["rn"])))
 
-print(atlas_ids)
+eq_list = []
+# loop over the atlas ids
+for i, atlas_id in enumerate(atlas_ids):
+    # print(f"atlas_ids {atlas_id}")
+    all_reactions = data[data['rn'] == atlas_id]
+    # Get only the forward
+    f_react = all_reactions[all_reactions['direction'] == "forward"]
+    # Get the reverse
+    r_react = all_reactions[all_reactions['direction'] == "reverse"]
+    if len(f_react) != 0:
+        item = f_react.values.tolist()
+    elif len(r_react) != 0:
+        item = r_react.values.tolist()
+    else:
+        print(f"Skipping {atlas_id} due to no forward or reverse")
+        continue
+    # Get the reactants and products
+    lhs = []
+    rhs = []
+    for j, entry in enumerate(item):
+        if entry[4] < 0:
+            lhs.append(f"{int(abs(entry[4]))} {entry[3]}")
+        else:
+            rhs.append(f"{int(abs(entry[4]))} {entry[3]}")
+    lhs = " + ".join(lhs)
+    rhs = " + ".join(rhs)
+    eq = f"{lhs} <=> {rhs}"
+    eq_list.append(eq)
+
+# Write the data to a file
+outfile = r"C:\Users\louie\skunkworks\data\atlas_reactions.csv.zip"
+# make the dataframe from the eq_list and the atlas_ids
+df = pd.DataFrame([atlas_ids, eq_list], columns=["ID", "Reaction"])
+df.to_csv(outfile, compression='zip', encoding='utf-8')
