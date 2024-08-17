42f9b2d7723dbe8ed7e4a3755b20ead782237281
Louie Slocombe <louies@hotmail.co.uk>
2024-08-16 18:43:21 -0700

    updated to include atlas data pull

--- atlas_converter.py
+++ atlas_converter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def convert_atlas(in_file, out_file):
@@ -52,9 +53,90 @@ def convert_atlas(in_file, out_file):
     return None
 
 
+def cleanup_eq_line(eq_line):
+    eq_line = eq_line.replace(")", " ")
+    eq_line = eq_line.replace("(", "")
+    eq_line = eq_line.replace("<==>", " <==> ")
+    eq_line = eq_line.replace("+", " + ")
+    return eq_line
+
+
+def clean_kegg_atlas(in_file, out_file):
+    # open the file
+    with open(in_file, "r") as f:
+        # read the data
+        data = f.read()
+    # split the data by new lines
+    data = data.split("\n")
+    # init the lists
+    re_id = []
+    re_eq = []
+    re_chem_names = []
+    re_ec = []
+
+    # loop over the data
+    for i, line in enumerate(data):
+        # split the line by the delimiter
+        line = line.split(";")
+        # Get the reaction id
+        re_id.append(line[0])
+        # Get the reaction equation
+        re_eq.append(cleanup_eq_line(line[1]))
+        # Get the reaction name
+        re_chem_names.append(line[2].replace("|", ""))
+        # Get the reaction EC
+        re_ec.append(line[3].replace("|", ""))
+    # Store the data in a dataframe
+    df = pd.DataFrame({'id': re_id, 'reaction': re_eq, 'chemical_names': re_chem_names, 'ec': re_ec})
+    # Write the data to a file
+    df.to_csv(out_file, compression='zip', encoding='utf-8', index=False)
+    return None
+
+
+def clean_atlas(in_file, out_file):
+    # Open the file
+    with open(in_file, "r") as f:
+        # Read the data
+        data = f.read()
+    # split the data by new lines
+    data = data.split("\n")
+    # init the lists
+    re_id = []
+    re_kegg_id = []
+    re_eq = []
+    re_ec = []
+
+    # loop over the data
+    for i, line in enumerate(data):
+        # split the line by the delimiter
+        line = line.split(";")
+        # Get the reaction id
+        re_id.append(line[0])
+        # Get the KEGG reaction id
+        re_kegg_id.append(line[1])
+        # Get the reaction equation
+        re_eq.append(cleanup_eq_line(line[3]))
+        # Get the reaction EC
+        re_ec.append(line[4].replace("|", ""))
+    # Store the data in a dataframe
+    df = pd.DataFrame({'id': re_id, 'kegg_id': re_kegg_id, 'reaction': re_eq, 'ec': re_ec})
+    # Write the data to a file
+    df.to_csv(out_file, compression='zip', encoding='utf-8', index=False)
+    return None
+
+
 if __name__ == "__main__":
     print("Program start", flush=True)
-    infile = r"C:\Users\louie\skunkworks\data\atlas_reactions.csv"
-    outfile = r"C:\Users\louie\skunkworks\data\atlas_reactions.csv.zip"
-    convert_atlas(infile, outfile)
+    # infile = r"C:\Users\louie\skunkworks\data\atlas_reactions.csv"
+    # outfile = r"C:\Users\louie\skunkworks\data\atlas_reactions.csv.zip"
+    # convert_atlas(infile, outfile)
+
+    infile = r"C:\Users\louie\Downloads\atlas_kegg_reactions.dat"
+    outfile = r"Data\atlas_kegg_reactions.csv.zip"
+    clean_kegg_atlas(infile, outfile)
+
+    infile = r"C:\Users\louie\Downloads\atlas_reactions.dat"
+    outfile = r"Data\atlas_reactions.csv.zip"
+    clean_atlas(infile, outfile)
+
     print("Program end", flush=True)
