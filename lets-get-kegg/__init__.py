54955ff13c4df60ff534cbbaef68a9f569435b8d
Louie Slocombe <louies@hotmail.co.uk>
2024-11-20 11:06:44 -0700

    updated package init files

--- lets-get-kegg/__init__.py
+++ lets-get-kegg/__init__.py
@@ -1,4 +1,30 @@
-from .tools_files import (file_list,
-                          list_empty_folders,
-                          clean_empty_folders,
-                          file_list_all)
+from tools_files import (file_list,
+                         file_list_all,
+                         list_empty_folders,
+                         clean_empty_folders,
+                         remove_filepath,
+                         delete_files_substring,
+                         )
+
+from tools_mols import (sanitize_mol,
+                        standardize_mol,
+                        fix_r_group,
+                        get_chirality,
+                        mol_replacer,
+                        get_mol_descriptors)
+
+from tools_eq import (eq_to_dict,
+                      dicts_to_eq,
+                      compare_dict_keys,
+                      compare_dicts,
+                      compare_dict_values,
+                      get_formulas_from_ids,
+                      side_to_dict,
+                      eq_to_dict,
+                      dict_to_side,
+                      dicts_to_eq,
+                      strip_plus_x,
+                      get_ids_to_formulas,
+                      )
+
+__version__ = "0.0.01"
