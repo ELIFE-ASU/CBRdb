ae7c4f49612354a49c2f50408d3d97350528889b
camerianm <cmillsap@asu.edu>
2025-02-09 01:44:58 -0700

    changed input for fix_reactions_data in run_all to reflect de-duped compounds

--- examples/run_all.py
+++ examples/run_all.py
@@ -51,8 +51,10 @@ if __name__ == "__main__":
     datasets = CBRdb.merge_data_sets.dedupe_compounds()  # modifies all compound and reaction data files above; returns de-duped versions of them
     
     print("Fixing the reactions data...", flush=True) 
-    datasets['kegg_data_R_processed'] = CBRdb.fix_reactions_data(r_file=kegg_reactions_data + "_dedupedCs.csv")  
-    datasets['atlas_data_R_processed'] = CBRdb.fix_reactions_data(r_file=atlas_reactions_data + "_dedupedCs.csv")
+    datasets['kegg_data_R_processed'] = CBRdb.fix_reactions_data(r_file=kegg_reactions_data + "_dedupedCs.csv",
+                                                                 c_file="../data/kegg_data_C_deduped.csv")  
+    datasets['atlas_data_R_processed'] = CBRdb.fix_reactions_data(r_file=atlas_reactions_data + "_dedupedCs.csv",
+                                                                 c_file="../data/kegg_data_C_deduped.csv")  
 
     print("Combining and deduplicating the reactions data...", flush=True)
     datasets['reactions_joined'] = pd.concat(objs=[datasets['kegg_data_R_processed'], datasets['atlas_data_R_processed']], ignore_index=True)
