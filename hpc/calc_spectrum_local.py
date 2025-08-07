2ec968b2573151906c888ddeeccfde023b16929e
Louie Slocombe <louies@hotmail.co.uk>
2025-08-06 17:08:04 -0700

    updated parsing

--- hpc/calc_spectrum_local.py
+++ hpc/calc_spectrum_local.py
@@ -1,6 +1,6 @@
 import os
 
-import numpy as np
+import pandas as pd
 from rdkit import Chem as Chem
 
 import CBRdb
@@ -10,32 +10,54 @@ if __name__ == "__main__":
 
     file = "CBRdb_C"
 
-    base_dir = os.path.abspath(os.path.expanduser(f"../data/"))
+    base_dir = os.path.abspath(os.path.expanduser(f"../"))
     if not os.path.exists(base_dir):
         os.makedirs(base_dir)
 
-    data_dir = os.path.join(base_dir, f"{file}.dat")
+    data_dir = os.path.join(base_dir, f"{file}.csv")
     out_file = os.path.join(base_dir, f"{file}_out.dat")
 
-    # Load the data
-    smi_list = np.loadtxt(data_dir, dtype=str, comments=None)
+    print(data_dir, flush=True)
 
-    print(f"Number of SMILES: {len(smi_list)}", flush=True)
+    df = pd.read_csv(data_dir)
 
-    # Get the smiles
-    mols = [Chem.AddHs(Chem.MolFromSmiles(smi, sanitize=True)) for smi in smi_list]
+    # Select data that does not contain a star in the smiles
+    df = df[~df['smiles'].str.contains(r'\*')]
 
-    for i in range(len(smi_list)):
-        print(f"Processing molecule: {i + 1}/{len(smi_list)}, smi: {smi_list[i]}", flush=True)
-        mol = mols[i]
+    # sort by n_heavy_atoms
+    df = df.sort_values(by='n_heavy_atoms', ascending=True)
+    # n_heavy_max = 4
+    # df = df[df['n_heavy_atoms'] <= n_heavy_max]
+    # df = df[df['n_heavy_atoms'] >= 1]
 
+    print(df.columns)
+    id_list = df['compound_id'].to_list()
+    smi_list = df['smiles'].to_list()
+    n_data = len(id_list)
+    print(f"Number of SMILES: {n_data}", flush=True)
+
+    # Get the mols
+    mols = [Chem.MolFromSmiles(smi, sanitize=True) for smi in smi_list]
+
+    for i in range(n_data):
+        print(f"Processing molecule: {i + 1}/{n_data}, smi: {smi_list[i]}", flush=True)
+        atoms, charge, multiplicity = CBRdb.smi_to_atoms(smi_list[i])
         # Calculate the IR spectrum
-        data = CBRdb.calculate_vib_spectrum(mol)
+        data_ir, data_raman, _ = CBRdb.calculate_vib_spectrum(atoms,
+                                                              charge=charge,
+                                                              multiplicity=multiplicity)
+
+        if data_ir is None or data_raman is None:
+            print(f"Skipping molecule {i + 1}/{n_data} due to missing data.", flush=True)
+            continue
 
         # Add them to the list
-        freq = data['Frequency (cm^-1)'].to_list()
-        intensity = data['Intensity (km/mol)'].to_list()
-
-        smi = Chem.MolToSmiles(mol, isomericSmiles=True, kekuleSmiles=True, canonical=True)
+        freq = data_ir['Frequency (cm^-1)'].to_list()
+        eps_ir = data_ir['Epsilon'].to_list()
+        int_ir = data_ir['Intensity (km/mol)'].to_list()
+        int_ram = data_raman['Activity (A^4 amu^-1)'].to_list()
+        dep_ram = data_raman['Depolarization'].to_list()
         # Write the data to the shared file
-        CBRdb.write_to_shared_file(f"{i}; {smi}; {freq}; {intensity}\n", out_file)
+        out_str = f"{id_list[i]}; {smi_list[i]}; {freq}; {eps_ir}; {int_ir}; {int_ram}; {dep_ram} \n"
+        CBRdb.write_to_shared_file(out_str, out_file)
+        break
