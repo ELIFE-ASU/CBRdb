0908af826a99ce0efa6eb034e5c162751203a733
AstroBioReza <143143336+AstroBioReza@users.noreply.github.com>
2025-08-11 10:34:41 -0700

    Update AtomTracking.sh

--- examples/AtomTracking.sh
+++ examples/AtomTracking.sh
@@ -1,14 +1,14 @@
 #!/bin/bash --login
 #SBATCH --job-name=trace_atom
-#SBATCH --array=0-100
+#SBATCH --array=0-148456
 #SBATCH --time=0-4:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=htc
 #SBATCH --qos=public
-#SBATCH -o /home/mshahjah/AtomTracking/Logs/out/slurm.%A_%a.out
-#SBATCH -e /home/mshahjah/AtomTracking/Logs/err/slurm.%A_%a.err
+#SBATCH -o CBRdb/AtomTracking/Logs/out/slurm.%A_%a.out
+#SBATCH -e CBRdb/AtomTracking/Logs/err/slurm.%A_%a.err
 #SBATCH --mem=100G
 module conda activate rxn_env
-srun $HOME/.conda/envs/rxn_env/bin/python3.8 /home/mshahjah/AtomTracking/AtomTracking.py --row_index ${SLURM_ARRAY_TASK_ID} --dynamic_cofactors 
+srun $HOME/.conda/envs/rxn_env/bin/python3.8 CBRdb/AtomTracking(Enhanced).py --row_index ${SLURM_ARRAY_TASK_ID} --dynamic_cofactors 
