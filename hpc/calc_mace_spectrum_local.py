64f1cc5de746feba612141db3bba18430089d858
Louie Slocombe <louies@hotmail.co.uk>
2025-08-23 21:09:18 -0700

    added mace hpc support #163

---
+++ hpc/calc_mace_spectrum_local.py
@@ -0,0 +1,61 @@
+import os
+
+import pandas as pd
+
+import CBRdb
+
+if __name__ == "__main__":
+    print(flush=True)
+
+    file = "CBRdb_C"
+
+    base_dir = os.path.abspath(os.path.expanduser(f"../"))
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+
+    data_dir = os.path.join(base_dir, f"{file}.csv")
+    out_file = os.path.join(base_dir, f"{file}_out.dat")
+
+    print(data_dir, flush=True)
+    # Load the data
+    df = pd.read_csv(data_dir)
+
+    # Select data that does not contain a star in the smiles
+    df = df[~df['smiles'].str.contains(r'\*')]
+
+    # Sort by n_heavy_atoms
+    df = df.sort_values(by='n_heavy_atoms', ascending=True)
+    n_heavy_max = 4
+    n_heavy_min = 2
+    df = df[df['n_heavy_atoms'] <= n_heavy_max]
+    df = df[df['n_heavy_atoms'] >= n_heavy_min]
+
+    print(df.columns)
+    id_list = df['compound_id'].to_list()
+    smi_list = df['smiles'].to_list()
+    n_data = len(id_list)
+    print(f"Number of SMILES: {n_data}", flush=True)
+
+    for i in range(n_data):
+        print(f"Processing molecule: {i + 1}/{n_data}, smi: {smi_list[i]}", flush=True)
+        atoms, charge, multiplicity = CBRdb.smi_to_atoms(smi_list[i])
+        # Calculate the IR spectrum
+        data_ir, data_raman, _ = CBRdb.calculate_vib_spectrum(atoms,
+                                                              charge=charge,
+                                                              multiplicity=multiplicity)
+
+        if data_ir is None or data_raman is None:
+            print(f"Skipping molecule {i + 1}/{n_data} due to missing data.", flush=True)
+            continue
+
+        # Add them to the list
+        freq = data_ir['Frequency (cm^-1)'].to_list()
+        eps_ir = data_ir['Epsilon'].to_list()
+        int_ir = data_ir['Intensity (km/mol)'].to_list()
+        int_ram = data_raman['Activity (A^4 amu^-1)'].to_list()
+        dep_ram = data_raman['Depolarization'].to_list()
+        # Write the data to the shared file
+        out_str = f"{id_list[i]}; {smi_list[i]}; {freq}; {eps_ir}; {int_ir}; {int_ram}; {dep_ram} \n"
+        CBRdb.write_to_shared_file(out_str, out_file)
+        if i > 10:
+            break
---
+++ hpc/sub_sol_array_mace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=cbrdb
+##SBATCH --output=output_%A_%a.out   # Output file for each array task
+##SBATCH --error=error_%A_%a.err     # Error file for each array task
+#SBATCH --array=0-4                  # Array of 10 jobs (adjust to your range) 15200 17023
+#SBATCH --time=0-04:00:00            # Time limit
+#SBATCH --ntasks=1                   # Number of tasks (1 per array task)
+#SBATCH --cpus-per-task=8            # Number of CPU cores per task
+#SBATCH --mem=8G                     # Memory per task 4 or 16
+#SBATCH --partition=htc              # general lightwork highmem
+#SBATCH --qos=public                 # private public
+
+cd $SLURM_SUBMIT_DIR
+
+module load mamba/latest
+source activate cbrdb
+
+# Run the Python script, passing the SLURM_ARRAY_TASK_ID to the script
+srun python3 calc_spectrum.py "${SLURM_ARRAY_TASK_ID}" CBRdb_C.csv
