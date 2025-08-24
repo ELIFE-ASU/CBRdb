ae6ab653c5bf450fbe7186d611f6a88a091d3888
camerianm <cmillsap@asu.edu>
2025-08-23 20:51:54 -0700

    clean_atlas preserves more info from provided reaction comparisons, removes dG and dG_err due to quality concerns

--- CBRdb/atlas_converter.py
+++ CBRdb/atlas_converter.py
@@ -53,26 +53,50 @@ def clean_atlas(in_file="../../data/atlas_reactions.dat",
         print(in_file, flush=True)
         raise FileNotFoundError
 
-    # Open the file. For header description, see p.6: https://lcsb-databases.epfl.ch/pathways/atlas/files/ATLAS_UserGuide.pdf
-    df = (pd.read_table(in_file, header=None, sep=';', usecols=[0, 1, 3, 4, 5, 6],
-                        converters={5: lambda x: -1.0 * pd.to_numeric(x, errors='coerce')},
-                        # sign reversed from pdf above
-                        names=['id', 'kegg_id', 'reaction', 'reaction_rule', 'dG_kJ/mol', 'dG_err'])
-          .assign(id=lambda x: 'A' + x.id.astype(str).str.zfill(6)))  # format ATLAS reactions as: AXXXXXX
+    # The ATLAS database headers are listed on p.6 of this guide:
+    # https://lcsb-databases.epfl.ch/pathways/atlas/files/ATLAS_UserGuide.pdf
+    in_file_cols = ['id', 'kegg_id', 'x', 'reaction', 'reaction_rule', 
+               'dG_kJ/mol', 'dG_err', 'most_sim_kegg', 'bridgit_score']
 
-    # Extract each reaction's list of 3rd-level EC#s. Remove non-conforming EC#s (e.g. not just numbers and -).
-    rr = (df['reaction_rule'].str.replace('-rev)', '-(rev)').str.split('|').explode().str.rstrip('(rev)').to_frame()
-          .assign(format_ok=lambda x: x.reaction_rule.str.replace('.', '').str.replace('-', '').str.isnumeric())
-          .query('format_ok').groupby(level=0)['reaction_rule'].apply(lambda x: ' '.join(sorted(list(set(x))))))
-    df = df.join(rr.rename('ec'), how='left').drop('reaction_rule', axis=1)
+    # Open the file.
+    df = pd.read_table(in_file, header=None, sep=';', names=in_file_cols)
 
+    # Remove known KEGG entries if requested
+    if f_exclude_kegg:
+        df = df.query('kegg_id.isna()').drop('kegg_id', axis=1)
+    # Standardize the ID column format
+    df['id'] = 'A' + df['id'].astype(str).str.zfill(6)
+    # ATLAS only annotates ECs to the 3rd digit
+    df['ec'] = df['reaction_rule'].str.findall(r'(\d\.\d+\.\d+\.[-])')
     # Standardize format of reaction equation.
     df['reaction'] = df['reaction'].apply(cleanup_eq_line).apply(standardise_eq)
 
-    if f_exclude_kegg:
-        df = df.query('kegg_id.isna()').drop('kegg_id', axis=1)
+    tags = df['most_sim_kegg'].str.split(r'[|]|[/]').explode()
+    rheas = tags[tags.str.contains('RHEA')].str.split('RHEA:').explode()
+    rheas = rheas.replace('', pd.NA).dropna().str.strip(' ')
 
+    # extract relevant IDs from the 'most_sim_kegg' column (abbreviated 'msk')
+    df['msk_rhea'] = rheas.groupby(level=0).apply(list)
+    df['msk_ecs'] = df['most_sim_kegg'].str.findall(r'(\d\.\d+\.\d+\.\d+)')
+    df['msk_rns'] = df['most_sim_kegg'].str.replace('BR', '').str.findall(r'(R\d{5})')
+    df['msk_metacyc'] = df['most_sim_kegg'].str.findall(r'(RXN-\d+)')
+    df['msk_mnxr'] = df['most_sim_kegg'].str.findall(r'(MNXR\d+)')
+
+    for fxn in [set, list]:
+        df.update(df.filter(like='msk').map(fxn, na_action='ignore'))
+        df.update(df['ec'].map(fxn, na_action='ignore'))
+    
+    # dG signs in the guide above are reversed in the data we were granted.
+    # TODO: Calculate dG for a few rxns with extreme (+/-) reported dG. Diff signs, y/n?
+    # df['dG_kJ/mol'] = -1 * df['dG_kJ/mol'].astype(float)
+    # TODO: If we are to keep ATLAS's dG-related fields, should find out why some dG_err are negative.
+    df.drop(columns=['x', 'reaction_rule', 'dG_kJ/mol', 'dG_err'], inplace=True)
+
+    listlike_cols = ['ec', 'msk_rhea', 'msk_ecs', 'msk_rns', 'msk_metacyc', 'msk_mnxr']
+    printable_df = df.copy(deep=True)
+    for col in listlike_cols:
+        printable_df[col] = printable_df[col].map(' '.join, na_action='ignore')
     # Write the data to a file
-    reaction_csv(df, out_file)
+    reaction_csv(printable_df, out_file)
     print("data written to file", flush=True)
-    return df
+    return printable_df
