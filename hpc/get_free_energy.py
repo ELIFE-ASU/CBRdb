746fc61319f8166e196ea8af24345d7425eb5c29
Louie Slocombe <louies@hotmail.co.uk>
2025-09-11 17:30:13 -0700

    added rev and dG #121

--- hpc/get_free_energy.py
+++ hpc/get_free_energy.py
@@ -1,40 +1,37 @@
-import os
-import sys
-
-import pandas as pd
-from rdkit import Chem as Chem
+import numpy as np
 import pubchempy as pcp
 from equilibrator_api import ComponentContribution, Q_
-# import CBRdb
 
-# conda install -c conda-forge equilibrator-api
-# conda install -c conda-forge pubchempy
+import warnings
+warnings.filterwarnings("error")
+
+
+def get_dg_prime(eq_line, conditions='nstandard'):
+    cc = ComponentContribution()
+    # replace 'C' with 'kegg:C' to use KEGG IDs
+    eq_line = eq_line.replace('C', 'kegg:C')
+    # replace '<=>' with '=' for equilibrium
+    eq_line = eq_line.replace('<=>', '=')
+
+    try:
+        react_line = cc.parse_reaction_formula(eq_line)
+        rev = cc.ln_reversibility_index(react_line).value.m_as("dimensionless")
+        if conditions == 'standard':
+            dg = cc.standard_dg_prime(react_line)
+        else:
+            # assuming physiological conditions
+            dg = cc.physiological_dg_prime(react_line)
+        val = dg.value.m_as("kJ/mol")
+        error = dg.error.m_as("kJ/mol")
+    except Exception as e:
+        print(f"Error: {e}")
+        return np.nan, np.nan, np.nan
+
+    if error == 100000.0:
+        error= np.nan
+    return val, error, rev
 
 if __name__ == "__main__":
     print(flush=True)
-    smiles = "C1=CC=C(C=C1)C=O"  # example
-    hits = pcp.get_compounds(smiles, namespace="smiles")
-    print(hits[0].cid)
-
-    cc = ComponentContribution()
-    cc.p_h = Q_(7.4)
-    cc.p_mg = Q_(3.0)
-    cc.ionic_strength = Q_("0.25M")
-    cc.temperature = Q_("298.15K")
-    atpase_reaction = cc.parse_reaction_formula(
-        "bigg.metabolite:atp + bigg.metabolite:h2o = "
-        "bigg.metabolite:adp + bigg.metabolite:pi"
-    )
-    dG_prime = cc.standard_dg_prime(atpase_reaction)
-    print(f"ΔG'° = {dG_prime}")
-
-    dGm_prime = cc.physiological_dg_prime(atpase_reaction)
-    print(f"ΔG'm = {dGm_prime}")
-
-    dG_prime_value_in_kj_per_mol = dG_prime.value.m_as("kJ/mol")
-    dG_prime_error_in_kj_per_mol = dG_prime.error.m_as("kJ/mol")
-    print(
-        f"ΔG'° = {dG_prime_value_in_kj_per_mol:.1f} +/- "
-        f"{dG_prime_error_in_kj_per_mol:.1f} kJ/mol"
-    )
-    print(f"ln(Reversibility Index) = {cc.ln_reversibility_index(atpase_reaction)}")
+    eq_line = '2 C00084 <=> 1 C00466'
+    print(get_dg_prime(eq_line))
