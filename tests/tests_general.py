ebe2ef356751b3588af7fc0e77e95ae1a77cb73c
Louie Slocombe <louies@hotmail.co.uk>
2025-09-19 10:45:18 -0700

    added first stab at enum_ionization_states #175

--- tests/tests_general.py
+++ tests/tests_general.py
@@ -1049,3 +1049,23 @@ def test_get_eq_all_cids():
     cids = CBRdb.get_eq_all_cids(eq)
     print(cids)
     assert cids == ['C00001', 'C00027', 'C00080', 'C19610', 'C19611']
+
+
+def enum_ionization_states(smi, ph_min=4.0, ph_max=10.0, precision=1.0, label=True, max_n=15):
+    from dimorphite_dl import protonate_smiles
+
+    enum_list = protonate_smiles(smi,
+                            ph_min=ph_min,
+                            ph_max=ph_max,
+                            precision=precision,
+                            label_states=label,
+                            max_variants=max_n)
+
+    # Remove the input molecule from the list
+    return [s for s in enum_list if s != smi]
+
+
+def test_enum_prot_states():
+    smi = 'CC(=O)O'  # Acetic acid
+    prot_states = enum_ionization_states(smi)
+    print(prot_states)
